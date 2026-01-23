d6ac6a5c2f4cb4543e7a23c6b1799a2e4016d63e
Ghenghis <ghenghis@example.com>
2026-01-23 03:28:01 -0700

    fix(ci): add loguru dependency and fix IParameterSerializer nullability

--- src/Winhance.Core/Features/Common/Interfaces/IParameterSerializer.cs
+++ src/Winhance.Core/Features/Common/Interfaces/IParameterSerializer.cs
@@ -11,23 +11,23 @@ namespace Winhance.Core.Features.Common.Interfaces
         /// Serializes an object to a string representation.
         /// </summary>
         /// <param name="parameter">The object to serialize.</param>
-        /// <returns>A string representation of the object.</returns>
-        string Serialize(object parameter);
+        /// <returns>A string representation of the object, or null if parameter is null.</returns>
+        string? Serialize(object? parameter);
 
         /// <summary>
         /// Deserializes a string to an object of the specified type.
         /// </summary>
         /// <param name="targetType">The type to deserialize to.</param>
         /// <param name="value">The string to deserialize.</param>
-        /// <returns>The deserialized object.</returns>
-        object Deserialize(Type targetType, string value);
+        /// <returns>The deserialized object, or null if deserialization fails.</returns>
+        object? Deserialize(Type targetType, string? value);
 
         /// <summary>
         /// Deserializes a string to an object of the specified type.
         /// </summary>
         /// <typeparam name="T">The type to deserialize to.</typeparam>
         /// <param name="serialized">The string to deserialize.</param>
-        /// <returns>The deserialized object.</returns>
-        T Deserialize<T>(string serialized);
+        /// <returns>The deserialized object, or default if deserialization fails.</returns>
+        T? Deserialize<T>(string? serialized);
     }
 }
